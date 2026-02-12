863de34b1619dc13cbf07b5ba5d5c1db3be52a94
miracle boniface <udohmiracle23@gmail.com>
2026-02-12 22:34:09 +0100

    repo updated: admin api endpoints modified

--- services/Admin.ts
+++ services/Admin.ts
@@ -19,7 +19,7 @@ export async function updateAdminAPI(details: Partial<AdminType>): Promise<Admin
   return res.data
 }
 
-export async function getTotalUsersAPI(): Promise<number> {
+export async function getTotalUsersCountAPI(): Promise<number> {
   const res = await authFetch.get("/admin/totalUsers/")
   return res.data
 }
@@ -35,12 +35,49 @@ export async function getUsersAPI(limit = 50, page = 1): Promise<{
   return res.data
 }
 
-export async function viewUserAPI(email: string){
+export async function viewUserAPI(email: string): Promise<UserType> {
   const res = await authFetch.get(`/admin/user/${email}`)
   return res.data
 }
 
-export async function updateUserByAdminAPI(email: string, details: Partial<UserType>){
+export async function updateUserByAdminAPI(email: string, details: Partial<UserType>): Promise<UserType> {
   const res = await authFetch.patch(`/admin/user/${email}`, details)
+  return res.data
+}
+
+export async function getUserByUserIdAPI(userID: string): Promise<UserType> {
+  const res = await authFetch.get(`/admin/user?userID=${userID}`)
+  return res.data
+}
+
+export async function searchUserAPI(keyword: string): Promise<UserType> {
+  const res = await authFetch.get(`/admin/search/users?keyword=${keyword}`)
+  return res.data
+}
+
+
+export async function getTotalCrewCountAPI(): Promise<number> {
+  const res = await authFetch.get("/admin/totalCrews/")
+  return res.data
+}
+
+export async function getCrewsAPI(limit = 50, page = 1): Promise<{
+  crews: CrewType[],
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+}> {
+  const res = await authFetch.get(`/admin/crews?limit=${limit}&page=${page}`)
+  return res.data
+}
+
+export async function getUserCrewAPI(userID: string): Promise<CrewType> {
+  const res = await authFetch.get(`/admin/crew?userID=${userID}`)
+  return res.data
+}
+
+export async function searchCrewAPI(keyword: string): Promise<CrewType> {
+  const res = await authFetch.get(`/admin/search/crews?keyword=${keyword}`)
   return res.data
 }
