f43b667402869dcf62958ac4b4dc6070eb7e7336
otobong23 <113153105+otobong23@users.noreply.github.com>
2026-03-09 13:44:50 +0100

    Adjust layout of HelpCenter form elements

--- app/(user)/dashboard/(dashboard_list)/help-center/HelpCenter.tsx
+++ app/(user)/dashboard/(dashboard_list)/help-center/HelpCenter.tsx
@@ -59,7 +59,7 @@ const HelpCenter = () => {
          <UI_header title='Help' description='Center' image={p1} containerStyle='items-center' />
 
          <form className="flex flex-col" onSubmit={handleSubmit}>
-            <div className='flex px-4 gap-3 mb-2.5'>
+            <div className='flex flex-col lg:flex-row px-4 gap-3 mb-2.5'>
                <div className='flex flex-col flex-1'>
                   <label htmlFor="name" className='mb-1.5'>Name</label>
                   <input required type="text" onChange={e => handleFormState("name", e.target.value)} value={formState['name']} name='name' id='name' placeholder='Loisbecket' className='placeholder:text-[#62686E] bg-[#F5F5F7]/7 px-3.5 py-3 outline-0' />
