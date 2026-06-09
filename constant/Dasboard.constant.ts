00c7b88f5aa91f820928b6761e5398c012e768f4
otobong23 <113153105+otobong23@users.noreply.github.com>
2026-06-09 18:51:35 +0100

    Update Message Center link in help_center

--- constant/Dasboard.constant.ts
+++ constant/Dasboard.constant.ts
@@ -65,6 +65,6 @@ export const security_center = [
 ]
 
 export const help_center = [
-   { name: 'Message Center', href: 'https://t.me/FERRIXAGENT011', icon: 'mdi:message' },
+   { name: 'Message Center', href: 'https://wa.me/447853159845', icon: 'mdi:message' },
    { name: 'Call Customer Care', href: 'https://t.me/FERRIXAGENT011', icon: 'ri:customer-service-fill' },
 ]
