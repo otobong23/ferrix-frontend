942b943d41d30f8460b90a551e85549fbac6abb5
miracle boniface <udohmiracle23@gmail.com>
2026-06-19 09:09:46 +0100

    admin Transaction page modified

--- app/admin/(protected)/transaction/Transaction.tsx
+++ app/admin/(protected)/transaction/Transaction.tsx
@@ -60,7 +60,7 @@ const Transaction = () => {
         status = undefined;
       }
 
-      const res = await getTransactionsAPI(10, page, type, status);
+      const res = await getTransactionsAPI(25, page, type, status);
       let results = res.transactions;
 
       if (filter.title === 'New') {
@@ -198,6 +198,7 @@ const Transaction = () => {
                 handleClick={handlebutton}
                 email={a.email}
                 type={a.type}
+                displayAmount={a.displayAmount}
                 amount={a.amount}
                 updatedAt={a.updatedAt ?? ''}
                 walletAddress={a.withdrawWalletAddress}
@@ -274,7 +275,7 @@ const Pending = ({ email, image, type, amount, updatedAt, handleClick, _id, wall
           <p>{formatTime(updatedAt)}</p>
         </div>
         <div className={`flex flex-col gap-2 mt-5 justify-end max-w-full overflow-hidden transition-all duration-300 ${toggle ? 'max-h-40' : 'max-h-0'}`}>
-          {displayAmount && (<div className='flex text-[#F5F5F7] overflow-hidden'>
+          {displayAmount && type == "withdrawal" && (<div className='flex text-[#F5F5F7] overflow-hidden'>
             <p className='w-[250px] relative truncate'>Pay Amount: <span>{displayAmount}</span></p>
             <button className='cursor-pointer' onClick={() => oncopy(String(displayAmount))}>
               <Icon icon='akar-icons:copy' className='text-[20px]' />
